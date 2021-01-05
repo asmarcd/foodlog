512599d7a65cb85c2317e769d0b9a5cd5aa7bb15
Chris Asmar <asmarcd@gmail.com>
2021-01-05 14:22:42 -0800

    trying to get testing working

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do 
+      entry = Entry.new(mean_type: "Breakfast", carbohydrates: 10, proteins: 20, fats: 30, calories: 40)
+      assert entry.save
+  end
 end
